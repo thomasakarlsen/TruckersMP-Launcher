409713b1249932714725c991bba86ccbe1049c61
Thomas Andre Karlsen <tho1233@gmail.com>
2016-04-06 17:30:05 +0200

    Hotfix to ALPHA 0.08
    Hotfix of auto updater

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -88,7 +88,7 @@ namespace truckersmplauncher
             string gotVer = (string)latestver["Version"];
             string localVer = Properties.Settings.Default.LauncherVersion;
 
-            if ( string.Compare(gotVer, localVer, true) > 0)
+            if ( !(string.Compare(gotVer, localVer, true) > 0))
             {
                 MessageBox.Show("Current version is: " + Properties.Settings.Default.LauncherVersion + "\nLatest version is: " + (string)latestver["Version"] + "\n\nYou already have the newest version!\nThere is no need for you to update!", "TruckersMP Launcher", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
